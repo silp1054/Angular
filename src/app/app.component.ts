85a70c0e80f71f66b5bf80447c3b72b70d676467
silp1054 <silp1054@gmail.com>
2017-11-09 11:53:30 -0600

    actualizacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title: string;
+  name: string;
+  email:string;
+  website: string;
+  temas: string[];
+  showTemas: boolean;
+
+  constructor() {
+    this.title= "Angular";
+    this.name= "ArchLinux";
+    this.email= "hola@ctrl-x.com.mx";
+    this.website= "http://ctrl-x.com.mx";
+    this.temas= ['Datos','Terror','Musica','Videos','Cine'];
+    this.showTemas= false;
+  }
+
+  toggleTemas(){
+    this.showTemas = !this.showTemas;
+  }
+
+  newTema(temas) {
+    this.temas.push(temas.value);
+    tema.value = '';
+    return false;
+  }
 }
